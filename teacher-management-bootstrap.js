cf285fcbefab8184bf2a7ec0fcb1e66158b4026e
Ahmedwalimohamed <ahmedmohamedhseed@gmail.com>
2026-09-22 21:15:15 +0300

    feat(a1): register Gold Lesson 1 API bootstrap

--- teacher-management-bootstrap.js
+++ teacher-management-bootstrap.js
@@ -50,5 +50,5 @@ express.application.post=function teacherManagementPost(route,...handlers){
   return originalPost.call(this,route,...handlers);
 };
 
-require('./reading-listening-separation-bootstrap.js');
+require('./a1-gold-bootstrap.js');\nrequire('./reading-listening-separation-bootstrap.js');
 require('./ai-content-editor-bootstrap.js');
